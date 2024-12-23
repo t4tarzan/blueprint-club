4ccfa599adaa4624e7b657cd81caeaa5e42d4922
t4tarzan <rao.vinu@gmail.com>
2024-12-24 04:59:01 +0530

    fix: simplify sitemap generation to avoid database access during build

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,13 +1,6 @@
 import { GetServerSideProps } from 'next';
-import { prisma } from '@/lib/prisma';
 
-interface Team {
-  id: string;
-  slug: string;
-  updatedAt: Date;
-}
-
-function generateSiteMap(teams: Team[]) {
+function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
@@ -16,30 +9,21 @@ function generateSiteMap(teams: Team[]) {
      <url>
        <loc>${process.env.NEXTAUTH_URL}/teams</loc>
      </url>
-     ${teams
-       .map(({ slug, updatedAt }) => {
-         return `
-       <url>
-           <loc>${`${process.env.NEXTAUTH_URL}/teams/${slug}`}</loc>
-           <lastmod>${updatedAt.toISOString()}</lastmod>
-       </url>
-     `;
-       })
-       .join('')}
+     <url>
+       <loc>${process.env.NEXTAUTH_URL}/about</loc>
+     </url>
+     <url>
+       <loc>${process.env.NEXTAUTH_URL}/contact</loc>
+     </url>
+     <url>
+       <loc>${process.env.NEXTAUTH_URL}/pricing</loc>
+     </url>
    </urlset>
  `;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const teams = await prisma.team.findMany({
-    select: {
-      id: true,
-      slug: true,
-      updatedAt: true,
-    },
-  });
-
-  const sitemap = generateSiteMap(teams);
+  const sitemap = generateSiteMap();
 
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
